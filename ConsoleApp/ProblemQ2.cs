e15053dc8cd01ac7ea4a9cdfca0470f5de60c129
AhmedChi <ahmedar61@gmail.com>
2020-09-04 14:14:04 +0100

    Completed

---
+++ ConsoleApp/ProblemQ2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class ProblemQ2
+    {
+        public List<string> MexicanWave(string word)
+        {
+            List<string> mexWaveList = new List<string>() { };
+
+            if (word.Length == 0)
+            {
+                Console.WriteLine("No phrase entered");
+
+                return mexWaveList;
+            }
+            else
+            {
+
+                for (var i = 0; i < word.Length; i++)
+                {
+                    string capLetter = word[i].ToString().ToUpper();
+
+                    if (capLetter != " ")
+                    {
+                        mexWaveList.Add(word.Remove(i, 1).Insert(i, capLetter));
+                    }
+                        
+                }
+
+                mexWaveList.ForEach(a =>
+                {
+                    Console.WriteLine(a);
+                });
+
+                return mexWaveList;
+            }
+                           
+        }
+    }
+}
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,11 +9,13 @@ namespace ConsoleApp
         {
             IStringMethods stringMethods = new StringMethods();
 
-            ProblemQ1 q1 = new ProblemQ1(stringMethods);
-            string phrase = "The quick brown fox jumps over the lazy dog.";
-            q1.GreaterThan5(phrase);
+            /* ProblemQ1 q1 = new ProblemQ1(stringMethods);
+            string q1_phrase = "The quick brown fox jumps over the lazy dog.";
+            q1.GreaterThan5(q1_phrase);*/
 
-            
+            ProblemQ2 q2 = new ProblemQ2();
+            string q2_phrase = "Two words";
+            q2.MexicanWave(q2_phrase);
             
             
             Console.ReadLine();
